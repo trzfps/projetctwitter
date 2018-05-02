d7edc030c5d0e01ec7099f0ecba6da768cbf15f0
Vitor <vitor@superaremedia.com.br>
2018-05-02 17:11:03 -0300

    alteraçoes

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,7 +9,37 @@
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
+        //verificar se o usuario já existe
+        $sql = " select * from usuarios where usuario = '$usuario' ";
+        if($resultado_id = mysqli_query($link,$sql)){
 
+            $dados_usuario =  mysqli_fetch_array($resultado_id);
+
+            if(isset($dados_usuario['usuario'])){
+                echo "Usuário já cadastrado";
+            }else {
+                echo "úsuario não cadastrado";
+            }
+            
+        }else {
+            echo "Erro ao tentar localizar o registro de úsuario";
+        }
+        //verificar se email já existe
+        $sql = " select * from usuarios where email = 'email' ";
+        if($resultado_id = mysqli_query($link,$sql)){
+
+            $dados_usuario =  mysqli_fetch_array($resultado_id);
+
+            if(isset($dados_usuario['email'])){
+                echo "Email já cadastrado";
+            }else {
+                echo "Email não cadastrado";
+            }
+            
+        }else {
+            echo "Erro ao tentar localizar o registro de email";
+        }
+        die();
     $sql = " insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha') ";
 
     //executar query 
