95e3ce5d04d50ddabc37dc4c0b6cea8d8a1d8789
Vitor <vitor@superaremedia.com.br>
2018-05-03 14:11:25 -0300

    alterações

--- home.php
+++ home.php
@@ -48,20 +48,45 @@
 	    	
 	    	<br /><br />
 
-	    	<div class="col-md-4"></div>
-	    	<div class="col-md-4">
-	    		<h3>Usuário autenticado.</h3>
-                <?= $_SESSION['usuario'] ?>
-                <?= $_SESSION['email'] ?>
+	    	<div class="col-md-3">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<h4><?= $_SESSION['usuario'] ?></h4>
+
+							<hr />
+							<div class="col-md-6">
+								TWEETS <br /> 1
+							</div>
+							<div class="col-md-6">
+								SEGUIDORES <br/> 1
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+	    	<div class="col-md-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="O que está acontecendo agora?" maxlength="140">
+								<span class="input-group-btn">
+									<button class="btn btn-default" type="button">Tweet</button>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<h4><a href="#">Procurar por pessoas</a></h4>
+						</div>
+					</div>
+				</div>
 
 			</div>
-			<div class="col-md-4"></div>
 
-			<div class="clearfix"></div>
-			<br />
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
 
 		</div>
 
