97d5fd0efa2a7d95969ee0c296944c3e01160034
Vitor <vitor@superaremedia.com.br>
2018-05-03 18:31:31 -0300

    alterações

--- get_tweet.php
+++ get_tweet.php
@@ -12,7 +12,9 @@
     $objDb = new db();
     $link = $objDb->conecta_mysql();
 
-    $sql = " SELECT * FROM tweet WHERE id_usuario = $id_usuario ORDER BY data_inclusao DESC";
+    $sql = "SELECT t.data_inclusao, t.tweet, u.usuario ";
+    $sql.=" FROM tweet AS t JOIN usuarios AS u ON (t.id_usuario = u.id) ";
+    $sql.= " WHERE id_usuario = $id_usuario ORDER BY data_inclusao DESC";
 
     
 
@@ -21,7 +23,7 @@
      if($resultado_id){
          while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
              echo '<a href="#" class="list-group-item">';
-                echo '<h4 class="list-group-item-heading">Nome<small> - '.$registro['data_inclusao'].'</small></h4> ';
+                echo '<h4 class="list-group-item-heading">'.$registro['usuario'].'<small> - '.$registro['data_inclusao'].'</small></h4> ';
                 echo '<p class="list-group-item-text">'.$registro['tweet'].'</p>';
 
              echo '</a>';
